91521afc7e12f7ec1365b3e015df621836ab0351
Satapitchakorn Punjarat <satapitchakorn.punjarat@allianz.com>
2020-07-30 22:12:32 +0700

    Change method.

--- frontend/src/app/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/shopping-cart/shopping-cart.component.ts
@@ -3,6 +3,7 @@ import { CartItem } from '../models/cart-item';
 import { ProductServiceService } from '../services/product-service.service';
 import { Product } from '../models/product';
 import { Router } from '@angular/router';
+import { ShoppingServiceService } from '../services/shopping-service.service'
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
@@ -12,31 +13,33 @@ export class ShoppingCartComponent implements OnInit {
 
   productCart = new CartItem
   total: number = 0
-  constructor(private api: ProductServiceService, private router: Router) {
-    api.getAllProduct().subscribe((i: Product[]) => {
-      this.productCart.products = i
-      i.forEach(p => {
+  constructor(private api: ProductServiceService, private router: Router, private store: ShoppingServiceService) {
+    store.getItems().subscribe(i => {
+      this.productCart = i
+      i.products.forEach(p => {
         p.amount = p.quantity
         this.total += p.price * p.amount
       });
-    })
+    });
   }
   array(n: number): any[] {
     return Array(n);
   }
   ngOnInit(): void {
   }
-  updatePrice() {
+  updatePrice(): void {
     this.total = 0
-    this.productCart.products.forEach(p => {
-      this.total += p.price * p.amount
-    });
+    this.store.getItems().subscribe(i => {
+      i.products.forEach(p => {
+        this.total += p.price * p.amount
+      });
+    })
   }
-  onDelete(id: number) {
-    this.productCart.products = this.productCart.products.filter(product => product.id !== id)
+  onDelete(id: number): void {
+    this.store.removeItem(id)
     this.updatePrice()
   }
-  onSubmit() {
+  onSubmit(): void {
     this.updatePrice()
     this.router.navigateByUrl('/shipping');
   }
