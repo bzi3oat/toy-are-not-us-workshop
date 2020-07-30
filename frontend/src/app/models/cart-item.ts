6180a763b232cfec55791a54fa4dc9663a21ae87
Phongpak Taothong <phongpaktao@mathregit.group>
2020-07-30 14:24:45 +0700

    Fix service

--- frontend/src/app/models/cart-item.ts
+++ frontend/src/app/models/cart-item.ts
@@ -3,4 +3,6 @@ import { Product } from './product';
 export class CartItem {
     public products: Product[];
     public totalPrice: number;
+    public shipping: string;
+    public shippingFee: number;
 }
