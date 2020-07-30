bcae2801a0babae4e275499fb3cc95d23adede96
Phongpak Taothong <phongpaktao@mathregit.group>
2020-07-30 16:37:39 +0700

    Update robot [Phongpak]

--- frontend/src/app/search-page/search-page.component.ts
+++ frontend/src/app/search-page/search-page.component.ts
@@ -8,8 +8,8 @@ import { ProductServiceService } from '../services/product-service.service';
   styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent implements OnInit {
-  ageOption: string="any";
-  genderOption: string="any";
+  ageOption: string = "any";
+  genderOption: string = "any";
   products: Product[] = [];
   constructor(private router: Router, private service: ProductServiceService) { }
 
@@ -19,25 +19,33 @@ export class SearchPageComponent implements OnInit {
     });
   }
 
-  searchProduct(age, gender){
-    if (age == "any" && gender == "any"){
+  searchProduct(age, gender) {
+    if (age == "any" && gender == "any") {
       this.service.getAllProduct().subscribe((products) => {
         return (this.products = products);
       });
     }
-    else if (age == "any"){
+    else if (age == "any") {
       this.service.getAllProduct().subscribe((products) => {
-        return (this.products = products.filter((p) =>{return p.gender==gender;}));
-      });}
-    else if (gender =="any"){
+        return (this.products = products.filter((p) => { return p.gender == gender; }));
+      });
+    }
+    else if (gender == "any") {
       this.service.getAllProduct().subscribe((products) => {
-        return (this.products = products.filter((p) =>{return p.age == age}));
-      });}
+        return (this.products = products.filter((p) => { return p.age == age }));
+      });
+    }
     else {
-    this.service.getAllProduct().subscribe((products) => {
-      return (this.products = products.filter((p) =>{return p.age == age && p.gender==gender;}));
-    });}
+      this.service.getAllProduct().subscribe((products) => {
+        return (this.products = products.filter((p) => { return p.age == age && p.gender == gender; }));
+      });
+    }
   }
+
+  onClickProduct(productId: number) {
+    this.router.navigateByUrl(`/product/${productId}`);
+  }
+
 
 
 }
